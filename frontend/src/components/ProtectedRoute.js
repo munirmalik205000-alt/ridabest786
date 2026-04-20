0fcba43aa59ab1b945c18406e2ad53d5a655f381
munirmalik205000-alt <munir.malik205000@gmail.com>
2026-04-20 17:21:16 +0530

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -4,12 +4,10 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("user"));
 
-  // ❌ अगर user नहीं → login भेजो
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // ✅ अगर user है → page दिखाओ
   return children;
 };
 
