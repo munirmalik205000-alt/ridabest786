d27dcaca530da7887ed2610b319a6aa25611b6a7
munirmalik205000-alt <munir.malik205000@gmail.com>
2026-04-21 21:57:57 +0530

    Create firebase.js

---
+++ api/firebase.js
@@ -0,0 +1,15 @@
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FB_PROJECT_ID,
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+}
+
+const db = admin.firestore();
+
+export { db };
